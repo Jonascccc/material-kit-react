76e96684ad99775d6a5c5e86660109105aac8ae3
LeLe <17qj@queensu.ca>
2023-11-01 11:54:54 +0800

    Create event-impact page

---
+++ src/pages/eventData.js
@@ -0,0 +1,14 @@
+// eventData.js
+export const eventData = {
+  title: "Title 1",
+  description: "Description 1",
+  numOfXXX: 10,
+  teamMembers: [
+    {
+      id: 1,
+      name: 'John Doe',
+      position: 'CEO',
+      imageSrc: '/images/cat.jpg', // 请提供成员的头像图片路径
+    },
+  ],
+};
